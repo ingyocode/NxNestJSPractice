9a558c7f2fc79d7ff1bbef5ce245ef788f2533b6
정인교 <69563418+ingyocode@users.noreply.github.com>
2021-10-05 19:00:19 +0900

    feat: Add Swagger Module

--- nx-prac-workspace/apps/nx-prac-nest-js/src/main.ts
+++ nx-prac-workspace/apps/nx-prac-nest-js/src/main.ts
@@ -5,16 +5,21 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('Test Swagger')
+    .setDescription('Swagger For Testing')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://localhost:' + port + '/' );
   });
 }
 
