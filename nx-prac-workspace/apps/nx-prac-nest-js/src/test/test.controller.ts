a79db609ff422e9569a305a8597f32c96df8fa69
정인교 <69563418+ingyocode@users.noreply.github.com>
2021-10-20 19:16:57 +0900

    use file interceptor

--- nx-prac-workspace/apps/nx-prac-nest-js/src/test/test.controller.ts
+++ nx-prac-workspace/apps/nx-prac-nest-js/src/test/test.controller.ts
@@ -1,6 +1,10 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Get, Post, Res, UploadedFile, UseInterceptors, StreamableFile} from '@nestjs/common';
 import {ApiOperation, ApiTags} from "@nestjs/swagger";
 import {TestService} from "./test.service";
+import {FileInterceptor} from "@nestjs/platform-express";
+import {createReadStream} from "fs";
+import * as es from 'event-stream'
+import {join} from 'path';
 
 @Controller('test')
 export class TestController {
@@ -13,5 +17,22 @@ export class TestController {
   @Get()
   testGet() {
     return this.testservice.testMethod()
+  }
+  //10GB 이상의 파일에서 RangeError [ERR_INVALID_OPT_VALUE]: The value "10485760000" is invalid for option "size" 에러 발생. 해당 부분 수정하기.
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file) {
+    console.log(file);
+  }
+  @Get('download')
+  //send(): StreamableFile {
+  send() {
+    let count;
+    const file = createReadStream(join(process.cwd(), 'test.mov'))
+    file.on('data',function (data) {
+      //res.write(data)
+    })
+    console.log(file)
+    //return new StreamableFile(file);
   }
 }
