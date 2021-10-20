933c2da6805eb82610196301cc81272e2ccddb4c
정인교 <69563418+ingyocode@users.noreply.github.com>
2021-10-20 19:16:41 +0900

    ws setting

--- nx-prac-workspace/apps/nx-prac-nest-js/src/app.module.ts
+++ nx-prac-workspace/apps/nx-prac-nest-js/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TestModule } from './test/test.module';
+import { EventsGateway } from './test-gateway.gateway';
 
 @Module({
-  imports: [TestModule]
+  imports: [TestModule],
+  providers: [EventsGateway]
 })
 export class AppModule {}
--- nx-prac-workspace/apps/nx-prac-nest-js/src/main.ts
+++ nx-prac-workspace/apps/nx-prac-nest-js/src/main.ts
@@ -7,17 +7,12 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import {WsAdapter} from "@nestjs/platform-ws";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Test Swagger')
-    .setDescription('Swagger For Testing')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  const port = process.env.PORT || 3333;
+  app.useWebSocketAdapter(new WsAdapter(app))
+  const port = process.env.PORT || 3000;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' );
   });
